6548e096c17b82b4f1801bfc64537705e159d2a8
cabustillo13 <cabustillo13@hotmail.com>
2021-04-28 00:56:30 -0300

    [Add] Functionalities: Send and getHandlers

--- Flask/app.py
+++ Flask/app.py
@@ -10,6 +10,9 @@ def index():
     """Por defecto va directamente a la carpeta templates a buscar el archivo html"""
     return render_template("index.html")
 
+@socketio.on("message")
+def handleMessage(msg):
+    print("Message: " + msg)
 
 if __name__ == '__main__':
     socketio.run(app)
